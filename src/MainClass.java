7a6263d56bff88d5c02c2c933f667dd420f48a21
FixMyMind <testformygit@gmail.com>
2024-03-25 16:57:41 +0200

    test

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,21 +1,18 @@
+import java.util.Arrays;
+
 public class MainClass {
     public static void main(String[] args) {
-        String[][] bankNameAndUSDPrice = new String[Bank.NUMBER_OF_BANKS][Bank.NUMBER_OF_BANKS];
         Bank bankAndPrice = new Bank();
-        String[][] sortedArray = new String[Bank.NUMBER_OF_BANKS][Bank.NUMBER_OF_BANKS];
-        for (int i = 0; i < bankNameAndUSDPrice.length; i++) {
-            bankNameAndUSDPrice[i][i] = Bank.bankNamesArray[i] + bankAndPrice.buyPrice();
+        String[][] bankNameAndUSDPrice = new String[Bank.NUMBER_OF_BANKS][2];
+        int iteration = 0;
+        while (iteration < Bank.NUMBER_OF_BANKS) {
+            bankNameAndUSDPrice[iteration][0] = Bank.bankNamesArray[iteration];
+            bankNameAndUSDPrice[iteration][1] = Double.toString(bankAndPrice.buyPrice());
+            iteration++;
         }
-        for (int i = 0; i < bankNameAndUSDPrice.length; i++) {
-            for (int j = 0; j < bankNameAndUSDPrice.length; j++) {
-                if (bankNameAndUSDPrice[i][i] == bankNameAndUSDPrice[j][j]){
-
-                }
-
-            }
-        }
-        for (int i = 0; i < bankNameAndUSDPrice.length; i++) {
-            System.out.println(bankNameAndUSDPrice[i][i]);
+        for (int i = 0; i < Bank.NUMBER_OF_BANKS; i++) {
+            System.out.println(Arrays.toString(bankNameAndUSDPrice[i]));
         }
     }
 }
+
