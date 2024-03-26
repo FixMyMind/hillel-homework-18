61e80bb0404eab0c1ac813e0586693b44a5eac28
FixMyMind <testformygit@gmail.com>
2024-03-26 08:56:28 +0200

    Homework solution

--- src/MainClass.java
+++ src/MainClass.java
@@ -10,8 +10,9 @@ public class MainClass {
             bankNameAndUSDPrice[iteration][1] = Double.toString(bankAndPrice.buyPrice());
             iteration++;
         }
-        boolean bigger;
-        int result = 0;
+//        Перший варіант вирішення задачі без сортування, лише через находження найбільшого курсу
+//        boolean bigger;
+//        int result = 0;
 //        for (int i = 1; i < Bank.NUMBER_OF_BANKS; i++) {
 //            bigger = (int) ((Double.parseDouble(bankNameAndUSDPrice[result][1]) * 100) /
 //                    (Double.parseDouble(bankNameAndUSDPrice[i][1]) * 100)) > 0;
@@ -19,41 +20,33 @@ public class MainClass {
 //                result = i;
 //            }
 //        }
-        for (int i = 0; i < Bank.NUMBER_OF_BANKS; i++) {
-            System.out.println(Arrays.toString(bankNameAndUSDPrice[i]));
-        }
+
+//        for (int i = 0; i < Bank.NUMBER_OF_BANKS; i++) { // Вивід у консоль не сортованого масиву додано для тестування
+//            System.out.println(Arrays.toString(bankNameAndUSDPrice[i]));
+//        }
+//        Другий варіант вирішення завдання з додаванням сортування бульбашковим алгоритмом
         String temp;
         String temp1;
-        for (int i = 1; i < Bank.NUMBER_OF_BANKS; i++) {
-            if ((int) ((Double.parseDouble(bankNameAndUSDPrice[result][1]) * 100) /
-                    (Double.parseDouble(bankNameAndUSDPrice[i][1]) * 100)) < 0) {
-                temp = bankNameAndUSDPrice[result][1];
-                temp1 = bankNameAndUSDPrice[result][0];
-                bankNameAndUSDPrice[result][1] = bankNameAndUSDPrice[i][1];
-                bankNameAndUSDPrice[result][0] = bankNameAndUSDPrice[i][0];
-                bankNameAndUSDPrice[i][1] = temp;
-                bankNameAndUSDPrice[i][0] = temp1;
-                i = 1;
-                result = 0;
-            } else if () {
-
-
+        for (int i = 0; i < bankNameAndUSDPrice.length; i++) {
+            for (int j = bankNameAndUSDPrice.length - 1; j > i; j--) {
+                if ((int) ((Double.parseDouble(bankNameAndUSDPrice[i][1]) * 100) /
+                        (Double.parseDouble(bankNameAndUSDPrice[j][1]) * 100)) > 0) {
+                    temp = bankNameAndUSDPrice[i][1];
+                    temp1 = bankNameAndUSDPrice[i][0];
+                    bankNameAndUSDPrice[i][1] = bankNameAndUSDPrice[j][1];
+                    bankNameAndUSDPrice[i][0] = bankNameAndUSDPrice[j][0];
+                    bankNameAndUSDPrice[j][1] = temp;
+                    bankNameAndUSDPrice[j][0] = temp1;
+                }
             }
         }
-        System.out.println("На ринку є 14 банків з різними курсами доллара: ");
-        for (
-                int i = 0;
-                i < Bank.NUMBER_OF_BANKS; i++) {
-            System.out.
-
-                    println(Arrays.toString(bankNameAndUSDPrice[i]));
-        }
-        System.out.
-
-                println("Найкращий курс: ");
-        System.out.
-
-                println(Arrays.toString(bankNameAndUSDPrice[result]));
+//        for (int i = 0; i < Bank.NUMBER_OF_BANKS; i++) { Вивід у консоль сортованого масиву додано для тестування
+//            System.out.println(Arrays.toString(bankNameAndUSDPrice[i]));
+//        }
+        System.out.println("Найкращий курс у наступних банках: ");
+        System.out.println(Arrays.toString(bankNameAndUSDPrice[Bank.NUMBER_OF_BANKS - 1]));
+        System.out.println(Arrays.toString(bankNameAndUSDPrice[Bank.NUMBER_OF_BANKS - 2]));
+        System.out.println(Arrays.toString(bankNameAndUSDPrice[Bank.NUMBER_OF_BANKS - 3]));
     }
 }
 
