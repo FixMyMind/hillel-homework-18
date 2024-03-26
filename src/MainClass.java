6731cb105d148c848c477b789e8cef916ca0611b
FixMyMind <testformygit@gmail.com>
2024-03-26 09:13:39 +0200

    Homework solution

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public class MainClass {
+
     public static void main(String[] args) {
         Bank bankAndPrice = new Bank();
         String[][] bankNameAndUSDPrice = new String[Bank.NUMBER_OF_BANKS][2];
